46e5981b9b7ead2b4ea52d5247b0cbb6003656a1
筱晓的mac <freya@xiaoxiaodemacdeMacBook-Pro.local>
2026-02-12 08:25:22 +0000

    Chapter3 Exercise

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there patner!")
+    return HttpResponse("Rango says hey there partner! <br/><a href='/rango/about/'>About</a>")
+def about(request):
+    return HttpResponse("This tutorial has been put together by Yufei Wang. <br/><a href='/rango/'>Home</a>")
