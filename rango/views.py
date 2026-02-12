cd1e0754834567571a12d72ad9455d5201562e3f
筱晓的mac <freya@xiaoxiaodemacdeMacBook-Pro.local>
2026-02-12 15:53:43 +0000

    Chapter 4.1 and 4.2

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/><a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context=context_dict)
+
 def about(request):
     return HttpResponse("This tutorial has been put together by Yufei Wang. <br/><a href='/rango/'>Home</a>")
